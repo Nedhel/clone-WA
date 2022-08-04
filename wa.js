ebc214167e9a5a5295f0fc2a9f29324e92c5bb55
Carlos Zalpa <nedhel.90@gmail.com>
2022-08-04 16:45:35 -0500

    improvments

--- wa.js
+++ wa.js
@@ -8,12 +8,52 @@ class MsjObject {
         this.date = new Date();
     }
 }
+let conversations = new Map();
+conversations.set(
+    "Aria",
+    '[{"user":1,"msj":"Hola Como estas?","date":"2022-08-03T20:27:57.432Z"},{"user":1,"msj":"que estas haciendo","date":"2022-08-03T20:27:57.432Z"},{"user":2,"msj":"Hola hola, bien y tu?","date":"2022-08-03T20:27:57.432Z"},{"user":2,"msj":"iba a comer algo","date":"2022-08-03T20:27:57.432Z"}]'
+);
+conversations.set(
+    "Carlos",
+    '[{"user":1,"msj":"Que onda we","date":"2022-08-03T20:29:57.432Z"},{"user":1,"msj":"que te dijeron?","date":"2022-08-03T20:50:57.432Z"},{"user":2,"msj":"Hola hola, bien y tu?","date":"2022-08-03T20:27:57.432Z"},{"user":2,"msj":"iba a comer algo","date":"2022-08-03T20:27:57.432Z"}]'
+);
+conversations.set(
+    "Pedro",
+    '[{"user":1,"msj":"Que dices primo?","date":"2022-08-03T20:30:57.432Z"},{"user":1,"msj":"donde es eso?","date":"2022-08-03T21:10:57.432Z"},{"user":2,"msj":"Hola hola, bien y tu?","date":"2022-08-03T20:27:57.432Z"},{"user":2,"msj":"iba a comer algo","date":"2022-08-03T20:27:57.432Z"}]'
+);
+conversations.set(
+    "+524426847591",
+    '[{"user":1,"msj":"Quien eres?","date":"2022-08-03T20:40:57.432Z"},{"user":1,"msj":"no veo tu foto de perfil","date":"2022-08-03T21:15:57.432Z"},{"user":2,"msj":"Hola hola, bien y tu?","date":"2022-08-03T20:27:57.432Z"},{"user":2,"msj":"iba a comer algo","date":"2022-08-03T20:27:57.432Z"}]'
+);
+function chargeHead(element) {
+    let head = document.getElementById("info-head");
+    let headChildren = document.getElementById("info-head").children;
+    headChildren[0].src = `${element.target.getAttribute(
+        "data-user-name"
+    )}.png`;
+    headChildren[0].setAttribute(
+        "data-user-name",
+        `${element.target.getAttribute("data-user-name")}`
+    );
+    headChildren[1].innerHTML = `${element.target.getAttribute(
+        "data-user-name"
+    )}`;
+    headChildren[1].setAttribute(
+        "data-user-name",
+        `${element.target.getAttribute("data-user-name")}`
+    );
+    head.setAttribute(
+        "data-user-name",
+        `${element.target.getAttribute("data-user-name")}`
+    );
+}
 
-let ari =
-    '[{"user":1,"msj":"Hola Como estas?","date":"2022-08-03T20:27:57.432Z"},{"user":1,"msj":"que estas haciendo","date":"2022-08-03T20:27:57.432Z"},{"user":2,"msj":"Hola hola, bien y tu?","date":"2022-08-03T20:27:57.432Z"},{"user":2,"msj":"iba a comer algo","date":"2022-08-03T20:27:57.432Z"}]';
-function chargeMsjs(user) {
-    let msj = JSON.parse(user);
+function chargeMsjs(element) {
+    let name = element.target.getAttribute("data-user-name");
+    let msj = JSON.parse(conversations.get(name));
     let divMsj = document.getElementById("msjs");
+    divMsj.innerHTML = "";
+    divMsj.setAttribute("data-user-name", name);
     let divEachMsj = null;
     for (const iterator of msj) {
         divEachMsj = document.createElement("div");
@@ -24,6 +64,8 @@ function chargeMsjs(user) {
         divEachMsj.innerHTML = `${iterator.msj}`;
         divMsj.appendChild(divEachMsj);
     }
+    chargeHead(element);
+    saveMsjs();
 }
 function sendMsj(user, msj) {
     let divMsj = document.getElementById("msjs");
@@ -37,7 +79,15 @@ function sendMsj(user, msj) {
 function reciveMsj(user, msj) {
     const currentUser = document
         .getElementById("msjs")
-        .getAttribute("data-user-msjs");
+        .getAttribute("data-user-name");
     currentUser === user ? sendMsj(2, msj) : notification(user);
 }
 function notification(user) {}
+function saveMsjs() {}
+
+window.addEventListener("load", () => {
+    let contacts = document.getElementById("contacts-list").children;
+    for (const iterator of contacts) {
+        iterator.addEventListener("click", chargeMsjs);
+    }
+});
